8ff12b15b250aeb8d6cc81a1765d82c95b6c4f54
egor3101 <eszxdrtfc14@mail.ru>
2021-03-01 15:51:30 +0300

    added user registration without authentication

--- my_websiteproject/cutawayapp/forms.py
+++ my_websiteproject/cutawayapp/forms.py
@@ -7,3 +7,12 @@ class BlogForm(forms.ModelForm):
     class Meta:
         model = Blog
         fields = ('name', 'image', 'text', 'date')
+
+
+class UserForm(forms.ModelForm):
+    email = forms.CharField(max_length=100, required=True)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'first_name', 'last_name', 'email')
--- my_websiteproject/cutawayapp/views.py
+++ my_websiteproject/cutawayapp/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
-
-from cutawayapp.models import Blog
-from cutawayapp.forms import BlogForm
-
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from cutawayapp.models import Blog
+from cutawayapp.forms import BlogForm, UserForm
 
 
 # Create your views here.
@@ -13,10 +15,12 @@ def home(request):
     return render(request, 'cutaway/home.html', {})
 
 
+@login_required(login_url='/cutaway/sign_in/')
 def algorithms(request):
     return render(request, 'cutaway/algorithms.html', {})
 
 
+@login_required(login_url='/cutaway/sign_in/')
 def blog(request):
     posts = Blog.objects.all().order_by('name')
     return render(request, 'cutaway/blog.html', {
@@ -36,4 +40,26 @@ def do_post(request):
     return render(request, 'cutaway/do_post.html', {
         'blog_form': blog_form,
         'sent': request.GET.get('sent', False)
+    })
+
+
+def sign_up(request):
+    user_form = UserForm()
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+
+        if user_form.is_valid():
+            new_user = User.objects.create_user(**user_form.cleaned_data)
+            new_user.save()
+
+            login(request,authenticate(
+                username=user_form.cleaned_data['username'],
+                password=user_form.cleaned_data['password']
+            ))
+
+            return redirect(home)
+
+    return render(request, 'cutaway/sign_up.html', {
+        'user_form': user_form
     })
