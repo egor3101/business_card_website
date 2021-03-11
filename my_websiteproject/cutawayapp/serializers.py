2fdba26f3e19d78f3757c2569891ff61bca01506
egor3101 <eszxdrtfc14@mail.ru>
2021-03-11 14:29:29 +0300

    setting the arrangement of objects in the blog

--- my_websiteproject/cutawayapp/serializers.py
+++ my_websiteproject/cutawayapp/serializers.py
@@ -5,10 +5,11 @@ from cutawayapp.models import Blog
 class BlogSerializers(serializers.ModelSerializer):
     image = serializers.SerializerMethodField()
 
-    def get_image(self,blog):
+    def get_image(self, blog):
         request = self.context.get('request')
-        image_url =blog.image.url
+        image_url = blog.image.url
         return request.build_absolute_uri(image_url)
+
     class Meta:
         model = Blog
         fields = ('id', 'name', 'text', 'image', 'date')
